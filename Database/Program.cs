69d73cf034bd2370f283ebfda09506f1955c0976
Nikko-Kostenko <70935960+Nikko-Kostenko@users.noreply.github.com>
2022-12-22 22:41:42 +0200

    Localhost version added
    Testing included

---
+++ Database/Program.cs
@@ -0,0 +1,11 @@
+﻿using EssentialDatabase.Core;
+using EssentialDatabase.CLI;
+using EssentialDatabase.Management;
+
+const string storeDbPath = @"C:\Users\Kostenko.Mykola\Documents\temp";
+
+PersistenceService persistenceService = new(storeDbPath);
+DatabaseManager databaseManager = new(persistenceService);
+
+CLIManager cliManager = new CLIManager(databaseManager);
+cliManager.StartConsoleReading();
